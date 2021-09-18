7232a18ef53ac62cfc81f9a4f21e392a4ddd27b5
Dom <72845136+53z@users.noreply.github.com>
2021-09-18 20:44:57 +1000

    bruh

--- main.py
+++ main.py
@@ -16,15 +16,20 @@ By RandomBackpack
 
 preview_image = True
 
-height = "10000"
-width = "4000"
+height = "1000"
+width = "400"
 skin_hash = "c99e2a73aa0cdb54"
 cape_hash = "8a6cc02cc86e43f1"
 slim = ""
-
+backwards_input = False
 skin_hash = input("Enter your skin hash:\n")
 
 slim_input = input("Would you like to render your skin in 'Slim' mode? [y/N]\n")
+
+if backwards_input == False:
+    backwards = ""
+else:
+    backwards = "&theta=210"
 
 if slim_input == "y":
     slim = "&model=slim"
@@ -35,9 +40,9 @@ if cape_input == "n":
     cape = ""
 else:
     cape_hash = input("Enter your cape hash:\n")
-    cape = f"&cape={cape_hash}&theta=210"
+    cape = f"&cape={cape_hash}"
 
-base_url = f"https://render.namemc.com/skin/3d/body.png?skin={skin_hash}{cape}{slim}&width={width}&height={height}"
+base_url = f"https://render.namemc.com/skin/3d/body.png?skin={skin_hash}{cape}{backwards}{slim}&width={width}&height={height}"
 
 response = requests.get(base_url)
 img = Image.open(BytesIO(response.content))
